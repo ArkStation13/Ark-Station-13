b5cbd93001f02842f4708f13210be1fbb78cadc3
NovaBot <154629622+NovaBot13@users.noreply.github.com>
2024-07-17 19:06:52 -0400

    [MIRROR] Ghosts once again can see people's true names and roundstart jobs [MDB IGNORE]

--- tgui/packages/tgui/interfaces/Orbit/JobIcon.tsx
+++ tgui/packages/tgui/interfaces/Orbit/JobIcon.tsx
@@ -4,6 +4,7 @@ import { Antagonist, Observable } from './types';
 
 type Props = {
   item: Observable | Antagonist;
+  realNameDisplay: boolean;
 };
 
 type IconSettings = {
@@ -22,7 +23,7 @@ const antagIcon: IconSettings = {
 };
 
 export function JobIcon(props: Props) {
-  const { item } = props;
+  const { item, realNameDisplay } = props;
 
   let iconSettings: IconSettings;
   if ('antag' in item) {
@@ -32,14 +33,24 @@ export function JobIcon(props: Props) {
   }
 
   // We don't need to cast here but typescript isn't smart enough to know that
-  const { icon = '', job = '' } = item;
+  const { icon = '', job = '', mind_icon = '', mind_job = '' } = item;
+  const usedIcon = realNameDisplay ? mind_icon || icon : icon;
+  const usedJob = realNameDisplay ? mind_job || job : job;
 
   return (
     <div className="JobIcon">
       {icon === 'borg' ? (
+<<<<<<< HEAD
         <Icon color="lightblue" name={JOB2ICON[job]} mr={0.5} />
       ) : (
         <div
+=======
+        <Icon color="lightblue" name={JOB2ICON[usedJob]} ml={0.3} mt={0.4} />
+      ) : (
+        <DmIcon
+          icon={iconSettings.dmi}
+          icon_state={usedIcon}
+>>>>>>> 9533a883146e... [MIRROR] Ghosts once again can see people's true names and roundstart jobs [MDB IGNORE] (#3842)
           style={{
             height: '17px',
             width: '18px',
